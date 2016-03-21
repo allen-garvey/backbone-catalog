f077f9f397892a6ea9d198cb56632c45bb8a6fb0
Allen Garvey <allengarvey@hotmail.com>
2016-03-20 22:04:21 -0400

    add basic routing on click

--- js_src/application.js
+++ js_src/application.js
@@ -2,22 +2,25 @@ var App = Marionette.Application.extend({
 	initialize: function(options) {
 		this.config = options.config;
         this.addRegions({
-  							mainRegion: "#main-content",
-  							headerRegion: "#universities-header",
-  							universityDescriptionRegion : '#university-description',
-  							courseListRegion : '#courses-container'
+							mainRegion: "#main-content",
+							headerRegion: "#universities-header",
+							universityDescriptionRegion : '#university-description',
+							courseListRegion : '#courses-container'
 						});
 		
-  	},
-  	start : function(){
-        this.universitiesController = new App.UniversitiesController(this);
-		
-  		Backbone.history.start();
-  		
-  	}
+	},
+	start : function(){
+        var universitiesController = new App.UniversitiesController(this);
+        this.universitiesController = universitiesController;
+        
+        this.universitiesRouter = new Marionette.AppRouter({
+                controller: universitiesController,
+                appRoutes: {
+                                "university/:id": "showUniversity"
+                            }
+        });
+		Backbone.history.start();
+	}
 });
-
-
-
 
 
--- js_src/controllers/universities.js
+++ js_src/controllers/universities.js
@@ -1,36 +1,44 @@
 App.UniversitiesController = function(app){
 	this.app = app;
-	this.currentUniversityIndex = 0;
 	this.universities = new App.UniversitiesCollection();
 	var promise = this.universities.fetch();
 	var controller = this;
 	promise.done(function(){
 		controller.universitiesView = new App.UniversitiesCollectionView({collection: controller.universities});
 		app.headerRegion.show(controller.universitiesView);
-		$('#universities-header').flipster({onItemSwitch: controller.universityClicked, start: controller.currentUniversityIndex });
-		controller.displayCurrentUniversity(controller.currentUniversityIndex);
+		$('#universities-header').flipster({onItemSwitch: controller.universityClicked, start: 0 });
+		controller.displayUniversity();
 	});
 
 	
-	this.displayCurrentUniversity = function(index){
-  		this.currentUniversityIndex = index;
-  		//display university description
-  		var university = this.universities.at(index);
-  		this.universityDescriptionView = new App.UniversityDescriptionView({model : university});
-  		this.app.universityDescriptionRegion.show(this.universityDescriptionView);
+	this.displayUniversity = function(university){
+        if(_.isUndefined(university)){
+            university = this.universities.at(0);
+        }
+        this.app.universitiesRouter.navigate('university/' + university.get('id'));
 
-  		var controller = this;
-  		university.actionForCourses(function(courses){
-  			var coursesView = new App.CoursesCollectionView({collection: courses});
-  			controller.app.courseListRegion.show(coursesView);
-  		});
-  	};
+    	this.universityDescriptionView = new App.UniversityDescriptionView({model : university});
+    	this.app.universityDescriptionRegion.show(this.universityDescriptionView);
 
-  	//can't use `this` because function is called from flipster jQuery plugin
-	this.universityClicked = function(currentItem, previousItem){
-  		var parentList = $('#universities-nav li');
-  		controller.displayCurrentUniversity(parentList.index(currentItem));
-  	}
+    	var controller = this;
+    	university.actionForCourses(function(courses){
+    		var coursesView = new App.CoursesCollectionView({collection: courses});
+    		controller.app.courseListRegion.show(coursesView);
+    	});
+    };
+
+    this.showUniversity = function(id){
+        var university = this.universities.get(id);
+        $('#universities-header').flipster('jump', this.universities.findIndex(university));
+        this.displayUniversity(university);
+    };
+
+    //can't use `this` because function is called from flipster jQuery plugin
+    this.universityClicked = function(currentItem, previousItem){
+    	var parentList = $('#universities-nav li');
+        var currentUniversity = controller.universities.at(parentList.index(currentItem));
+    	controller.displayUniversity(currentUniversity);
+    }
 };
 
 
